22a624d9f2a0fe1930de51f516188d6d2df80179
Oleksii Desiatnichenko <o.desyatnichenko@gmail.com>
2023-07-14 23:03:34 +0300

    Constructor parameters for attributes

--- Src/UIoC/Attributes/TypeAttribute.cs
+++ Src/UIoC/Attributes/TypeAttribute.cs
@@ -3,6 +3,12 @@
 namespace UIoC {
   [AttributeUsage(AttributeTargets.Class)]
   public class TypeAttribute : Attribute {
+    public TypeAttribute(Type resolveType = null, string resolveName = null) {
+      ResolveType = resolveType;
+      ResolveName = resolveName;
+    }
+    public TypeAttribute(string resolveName) : this(null, resolveName) {
+    }
     public Type ResolveType { get; set; }
     public string ResolveName { get; set; }
   }
