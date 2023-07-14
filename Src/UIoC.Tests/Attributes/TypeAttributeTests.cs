7f2d8252bde74c49afed7f62fa4bae78787dd264
Oleksii Desiatnichenko <o.desyatnichenko@gmail.com>
2023-07-14 23:17:52 +0300

    Tests for attribute constructor parameters

--- Src/UIoC.Tests/Attributes/TypeAttributeTests.cs
+++ Src/UIoC.Tests/Attributes/TypeAttributeTests.cs
@@ -5,30 +5,64 @@ namespace UIoC.Tests.Attributes {
   [TestClass]
   public class TypeAttributeTests {
 
+    internal interface I {
+      public string StrProp { get; set; }
+    }
+
     [Type]
-    internal class C {
-      public int IntProp { get; set; } = 0;
+    internal class C1 : I {
+      public string StrProp { get; set; } = "C1";
+    }
+
+    [Type(typeof(I))]
+    internal class C2 : I {
+      public string StrProp { get; set; } = "C2";
+    }
+
+    [Type("C3")]
+    internal class C3 : I {
+      public string StrProp { get; set; } = "C3";
     }
 
     [TestMethod]
-    public void Test() {
+    public void DefaultParameters() {
       IContainer container = new Container();
       container.AddFromAssembly(Assembly.GetExecutingAssembly());
 
 
-      var c1 = (C)container.Get(typeof(C), null);
+      var c1 = (I)container.Get(typeof(C1), null);
       Assert.IsNotNull(c1);
-      Assert.AreEqual(c1.IntProp, 0);
+      Assert.AreEqual(c1.StrProp, new C1().StrProp);
 
-      c1.IntProp++;
-      Assert.AreEqual(c1.IntProp, 1);
+      c1.StrProp += "new";
+      Assert.AreEqual(c1.StrProp, new C1().StrProp + "new");
 
 
-      var c2 = (C)container.Get(typeof(C), null);
+      var c2 = (I)container.Get(typeof(C1), null);
       Assert.IsNotNull(c2);
-      Assert.AreEqual(c2.IntProp, 0);
+      Assert.AreEqual(c2.StrProp, new C1().StrProp);
 
       Assert.AreNotEqual(c1, c2);
+    }
+
+    [TestMethod]
+    public void TypeParameters() {
+      IContainer container = new Container();
+      container.AddFromAssembly(Assembly.GetExecutingAssembly());
+
+      var c2 = (I)container.Get(typeof(I), null);
+      Assert.IsNotNull(c2);
+      Assert.AreEqual(c2.StrProp, new C2().StrProp);
+    }
+
+    [TestMethod]
+    public void NameParameters() {
+      IContainer container = new Container();
+      container.AddFromAssembly(Assembly.GetExecutingAssembly());
+
+      var c3 = (I)container.Get(typeof(C3), "C3");
+      Assert.IsNotNull(c3);
+      Assert.AreEqual(c3.StrProp, new C3().StrProp);
     }
   }
 }
